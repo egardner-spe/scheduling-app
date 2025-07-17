af9e8a7f02daaf11e0a88ed50a455457d3000ab7
Audree Moua <simplyaudree@gmail.com>
2025-07-17 01:35:28 -0400

    Update views.py

--- shifts/views.py
+++ shifts/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.utils.timezone import now
 from .models import TimeOffRequest, Shift, ShiftPool
 from .forms import TimeOffRequestForm
+from django.shortcuts import render
+
 
 @login_required
 def request_time_off(request):
@@ -35,8 +37,6 @@ def review_time_off(request, request_id, action):
 def admin_time_off_requests(request):
     requests = TimeOffRequest.objects.all()
     return render(request, 'shifts/admin_time_off_list.html', {'requests': requests})
-
-from django.shortcuts import render
 
 def home(request):
     return render(request, 'shifts/home.html')
