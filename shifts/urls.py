42acb4a8199f0bd941191d7e224601459c9c31e0
egardner-spe <disregard001@gmail.com>
2025-07-18 22:59:30 -0400

    dashboard availability updates

--- shifts/urls.py
+++ shifts/urls.py
@@ -37,5 +37,10 @@ urlpatterns = [
     path('admin/schedule/',               views.view_shift_schedule, name='view_shift_schedule'),
     path('admin/schedule/assign/<int:user_id>/<date:day>/', views.assign_shift, name='assign_shift'),
     path('admin/schedule/edit/<int:shift_id>/',      views.edit_shift,   name='edit_shift'),
+    path(
+        "admin/schedule/assign/<int:user_id>/<date:day>/",
+        views.assign_shift,
+        name="assign_shift",
+    ),
 
 ]
